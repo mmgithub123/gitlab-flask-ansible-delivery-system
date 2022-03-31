39990f46b6df6181bad4118ce0109fed035b5bb7
mmgithub123 <71955422+mmgithub123@users.noreply.github.com>
2022-03-31 19:57:50 +0800

    Update all_element_per_status_ansible.sh

--- script/all_element_per_status_ansible.sh
+++ script/all_element_per_status_ansible.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-ansible kkpost -m shell -a 'for i in {1..6};do sudo systemctl status kkpost@post$i;done' 
-ansible kkagg -m shell -a 'systemctl status kkagg' 
-ansible kkfront -m shell -a 'systemctl status kkfront' 
 ansible kkinterface -m shell -a 'systemctl status kkinterface' 
+ansible kkagg -m shell -a 'systemctl status kkagg' 
+ansible kkpost -m shell -a 'for i in {1..6};do sudo systemctl status kkpost@post$i;done' 
+
+
+
